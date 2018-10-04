13433612aff23398d464b4321f13736fd0c5480c
João Lucas Farias <joao@sistemasbr.net>
2018-10-04 15:03:35 -0300

    Primeiro rascunho do arquivo de atributos do campo do Tx2

---
+++ src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/Atributos/Tx2CampoAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SistemasBR.SerializadorTx2.Atributos
+{
+    public class Tx2CampoAttribute : Attribute
+    {
+        public Tx2CampoAttribute(string nomeCorrespondente, bool obrigatorio, int tamanhoMaximo)
+        {
+            NomeCorrespondente = nomeCorrespondente;
+            Obrigatorio = obrigatorio;
+            TamanhoMaximo = tamanhoMaximo;
+        }
+
+        public Tx2CampoAttribute(string nomeCorrespondente, bool obrigatorio)
+        {
+            NomeCorrespondente = nomeCorrespondente;
+            Obrigatorio = obrigatorio;
+        }
+
+        private string NomeCorrespondente { get; }
+        private bool Obrigatorio { get; }
+        private int TamanhoMaximo { get; }
+    }
+}
