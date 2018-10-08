9800090344a5141d9bf9c3d5d1724858b7bf9e32
João Lucas Farias <joao@sistemasbr.net>
2018-10-08 11:35:55 -0300

    Modificando classes de atributos. Retirando propriedades desnecessárias

--- src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/Atributos/Tx2CabecalhoAttribute.cs
+++ src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/Atributos/Tx2CabecalhoAttribute.cs
@@ -4,9 +4,8 @@ namespace SistemasBR.SerializadorTx2.Atributos
 {
     public class Tx2CabecalhoAttribute : Attribute
     {
-        internal string NomeCabecalho { get; }
-
-        public Tx2CabecalhoAttribute(string nomeCabecalho) =>
-            NomeCabecalho = nomeCabecalho;
+        public Tx2CabecalhoAttribute(string nomeCabecalho)
+        {
+        }
     }
 }
--- src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/Atributos/Tx2CampoAttribute.cs
+++ src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/Atributos/Tx2CampoAttribute.cs
@@ -4,21 +4,12 @@ namespace SistemasBR.SerializadorTx2.Atributos
 {
     public class Tx2CampoAttribute : Attribute
     {
-        internal string NomeCorrespondente { get; }
-        internal bool Obrigatorio { get; }
-        internal int TamanhoMaximo { get; }
-
         public Tx2CampoAttribute(string nomeCorrespondente, bool obrigatorio, int tamanhoMaximo)
         {
-            NomeCorrespondente = nomeCorrespondente;
-            Obrigatorio = obrigatorio;
-            TamanhoMaximo = tamanhoMaximo;
         }
 
         public Tx2CampoAttribute(string nomeCorrespondente, bool obrigatorio)
         {
-            NomeCorrespondente = nomeCorrespondente;
-            Obrigatorio = obrigatorio;
         }
     }
 }
