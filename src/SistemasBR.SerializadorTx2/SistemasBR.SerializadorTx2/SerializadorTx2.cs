42fa127a66d3dadc7561e8148d51728ca610d51d
João Lucas Farias <joao@sistemasbr.net>
2018-10-08 11:52:07 -0300

    Colocando as verificações padrões e modificando o código

--- src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/SerializadorTx2.cs
+++ src/SistemasBR.SerializadorTx2/SistemasBR.SerializadorTx2/SerializadorTx2.cs
@@ -24,31 +24,48 @@ namespace SistemasBR.SerializadorTx2
                 throw new CustomAttributeFormatException(
                     $"A classe deve conter o atributo \"{nameof(Tx2CabecalhoAttribute)}\" (Type: {typeof(Tx2CabecalhoAttribute)})");
 
-            var atributoCabecalhoClasse = atributosClasse.First();
+            var atributoTx2CabecalhoClasse = atributosClasse.First(a => a.AttributeType == typeof(Tx2CabecalhoAttribute));
 
-            foreach (var argumentosConstrutor in atributoCabecalhoClasse.ConstructorArguments)
+            foreach (var argumentosConstrutor in atributoTx2CabecalhoClasse.ConstructorArguments)
                 cabecalho = argumentosConstrutor.Value.ToString();
-
-
-
 
             var propriedades = tipo.GetProperties();
 
             foreach (var propriedade in propriedades)
             {
-                if (propriedade.CustomAttributes.All(a => a.AttributeType != typeof(Tx2CampoAttribute)))
+                var atributosProriedade = propriedade.CustomAttributes
+                                              as IList<CustomAttributeData> ?? propriedade.CustomAttributes.ToList();
+
+                if (atributosProriedade.All(a => a.AttributeType != typeof(Tx2CampoAttribute)))
                     continue;
 
-                foreach (var atributo in propriedade
-                    .CustomAttributes
-                    .Where(a => a.AttributeType == typeof(Tx2CampoAttribute)))
+                var atributoTx2Propriedade = atributosProriedade
+                    .First(a => a.AttributeType == typeof(Tx2CampoAttribute));
+
+                var argumentosConstrutorPropriedade = atributoTx2Propriedade.ConstructorArguments;
+
+                var nomeTx2 = argumentosConstrutorPropriedade[0].Value.ToString();
+
+                if (string.IsNullOrWhiteSpace(nomeTx2))
+                    throw new ArgumentNullException(nameof(nomeTx2));
+
+                var valorProriedade = propriedade.GetValue(objeto).ToString();
+
+                var preenchimentoObrigatorio = Convert.ToBoolean(argumentosConstrutorPropriedade[1].Value.ToString());
+
+                if (preenchimentoObrigatorio && string.IsNullOrWhiteSpace(valorProriedade.ToString()))
+                    throw new ArgumentNullException(nameof(propriedade));
+
+                if (argumentosConstrutorPropriedade.Count >= 3)
                 {
-                    foreach (var argumentosConstrutor in atributo.ConstructorArguments)
-                    {
-                        resposta += $"{argumentosConstrutor.Value}={propriedade.GetValue(objeto)}\n";
-                        break;
-                    }
+                    var quantidadeLimite = Convert.ToInt32(argumentosConstrutorPropriedade[2].Value.ToString());
+
+                    if (valorProriedade.Length > quantidadeLimite)
+                        throw new ArgumentOutOfRangeException(nameof(propriedade), valorProriedade,
+                            "Valor maior do que o permitido");
                 }
+
+                resposta += $"{nomeTx2}={valorProriedade}\n";
             }
 
             return $"INCLUIR{cabecalho}\n{resposta}SALVAR{cabecalho}";
